0344560365fe2522fb6590c86ab5facbaa0f97fc
Abdul-Rauf <80230949+abiola9525@users.noreply.github.com>
2023-01-25 03:34:27 -0600

    Update settings.py

--- user_management/settings.py
+++ user_management/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'user_management.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/user_management',
         conn_max_age=600
     )
 }
